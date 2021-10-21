d160cb2dda9b04adf710acbe2a94e85bfb020146
daniml3 <danimoral1001@gmail.com>
2021-10-21 18:31:56 +0200

    fixup! allapps: remove dynamic header background
    * Remove code leftovers
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I83ff2306419911e855b02840259a410dad236b9f

--- src/com/android/launcher3/allapps/AllAppsContainerView.java
+++ src/com/android/launcher3/allapps/AllAppsContainerView.java
@@ -88,8 +88,6 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
     public static final float PULL_MULTIPLIER = .02f;
     public static final float FLING_VELOCITY_MULTIPLIER = 1200f;
 
-    private final Paint mHeaderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
     protected final BaseDraggingActivity mLauncher;
     protected final AdapterHolder[] mAH;
     private final ItemInfoMatcher mPersonalMatcher = ItemInfoMatcher.ofUser(Process.myUserHandle());
@@ -133,7 +131,6 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
     private final int mHeaderProtectionColor;
     private final float mHeaderThreshold;
     private ScrimView mScrimView;
-    private int mHeaderColor;
     private int mTabsProtectionAlpha;
 
     public AllAppsContainerView(Context context) {
@@ -711,17 +708,6 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
 
     @Override
     public void drawOnScrim(Canvas canvas) {
-        mHeaderPaint.setColor(mHeaderColor);
-        mHeaderPaint.setAlpha((int) (getAlpha() * Color.alpha(mHeaderColor)));
-        if (mHeaderPaint.getColor() != mScrimColor && mHeaderPaint.getColor() != 0) {
-            int bottom = (int) (mSearchContainer.getBottom() + getTranslationY());
-            canvas.drawRect(0, 0, canvas.getWidth(), bottom, mHeaderPaint);
-            int tabsHeight = getFloatingHeaderView().getPeripheralProtectionHeight();
-            if (mTabsProtectionAlpha > 0 && tabsHeight != 0) {
-                mHeaderPaint.setAlpha((int) (getAlpha() * mTabsProtectionAlpha));
-                canvas.drawRect(0, bottom, canvas.getWidth(), bottom + tabsHeight, mHeaderPaint);
-            }
-        }
     }
 
     public class AdapterHolder {
